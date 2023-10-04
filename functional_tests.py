40cfe2b07397a1789ce9787169ea1983aab6eccd
itzRulzz <enotokot.001@gmail.com>
2023-10-04 21:54:19 +0300

    feat: added minimal HTML response in views.py

--- functional_tests.py
+++ functional_tests.py
@@ -15,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # Он видит, что в названии страницы и хэдере упомянуты To-do листы
-        self.assertIn('To-do', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
         # Коле предлагают с ходу написать задачу
